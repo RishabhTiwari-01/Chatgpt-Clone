8b59561ff9d1968e502918d7dbab5c3d907c94c9
Rishabh Tiwari <rt451495@gmail.com>
2026-02-21 17:59:32 +0530

    updted logout func

--- Fronted/src/Sidebar.jsx
+++ Fronted/src/Sidebar.jsx
@@ -43,7 +43,7 @@ const Sidebar = () => {
 
     const handleLogout = () => {
         localStorage.clear();
-        window.location.href = "/login";
+        window.location.href = "/";
     };
 
     return (
